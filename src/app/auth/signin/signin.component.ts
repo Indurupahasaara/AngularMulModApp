15b58efb2216e595070fa52116bf53d4aea6a2b4
Indurupahasara <indurupahasara.sl@gmail.com>
2022-05-27 20:38:19 +0530

    Signin Form Group Add

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  siginForm!:FormGroup;
+  constructor(private fb: FormBuilder) { 
+   }
 
   ngOnInit(): void {
+    this.siginForm = this.fb.group({
+      username: [''],
+      password: [''],   
+    });
   }
 
 }
